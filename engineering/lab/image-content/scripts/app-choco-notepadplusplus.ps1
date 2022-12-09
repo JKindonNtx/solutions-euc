5a1cb554ede1ba11c97e9d78843cf6b76e150565
James Kindon <James.Kindon@nutanix.com>
2022-12-09 11:52:27 +1100

    Application Additions

---
+++ engineering/lab/image-content/scripts/app-choco-notepadplusplus.ps1
@@ -0,0 +1,11 @@
+Write-Host "============================================================"
+Write-Host "===== Install notepad ++" -ForegroundColor "Green"
+Write-Host "============================================================"
+
+choco install notepadplusplus.install -Y --limit-output
+
+## The below is designed to allow pipeline continuation on failure - used for testing phases. Variable typically set in DevOps
+if ($LASTEXITCODE -ne "0" -and $Env:FailureOverrideCode -eq "0") {
+    write-Warning "Package Failed to Install. Continuing with false return code: 0"
+    Exit 0
+}
