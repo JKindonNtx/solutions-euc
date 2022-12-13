c1dc0036c7d043a98ec9dd9be8a9426afe0f1b43
jameskindon <James.Kindon@nutanix.com>
2022-12-13 17:16:42 +1100

    Fix build tasks

--- engineering/lab/image-content/scripts/app-choco-mozilla-firefox.ps1
+++ engineering/lab/image-content/scripts/app-choco-mozilla-firefox.ps1
@@ -2,7 +2,7 @@ Write-Host "============================================================"
 Write-Host "===== Install FireFox" -ForegroundColor "Green"
 Write-Host "============================================================"
 
-choco install firefox -Y --params "/l:en-US /NoTaskbarShortcut /NoDesktopShortcut /NoAutoUpdate" --limit-output
+choco install firefox -Y --params "/l:en-US /NoTaskbarShortcut /NoDesktopShortcut /NoAutoUpdate /RemoveDistributionDir" --limit-output
 
 ## The below is designed to allow pipeline continuation on failure - used for testing phases. Variable typically set in DevOps
 if ($LASTEXITCODE -ne "0" -and $Env:FailureOverrideCode -eq "0") {
