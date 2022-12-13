f320208dc85e06fe1c9c8975046c0cd0068ec684
David Brett <david.brett@nutanix.com>
2022-12-13 08:53:04 +0000

    cluster timezone edits

--- engineering/lab/image-content/scripts/cust-imageoptimize-citrix.ps1
+++ engineering/lab/image-content/scripts/cust-imageoptimize-citrix.ps1
@@ -5,7 +5,7 @@ Set-ExecutionPolicy Bypass -Force
 Set-MpPreference -DisableRealtimeMonitoring $True -ErrorAction SilentlyContinue
 
 $BuildSource = "C:\deployment"
-$CustomSource = "$BuildSource\custom"
+$CustomSource = "$BuildSource\custom\"
 
 $OptimizerRoot = "C:\Tools"
 $OptimizerHome  = "C:\Tools\CitrixOptimizer"
@@ -21,6 +21,6 @@ Expand-Archive -Path ($CustomSource + $OptimizerSource) -DestinationPath $Optimi
 #//  Remove execution if using BIS-F
 & "$OptimizerHome\CtxOptimizerEngine.ps1" -mode Execute
 #Use 3rd Party Optimizations
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/j81blog/Citrix_Optimizer_Community_Template_Marketplace/master/templates/John%20Billekens/JohnBillekens_3rd_Party_Components.xml" -UseBasicParsing -OutFile "$OptimizerHome\Templates\JohnBillekens_3rd_Party_Components.xml"
+# Invoke-WebRequest -Uri "https://raw.githubusercontent.com/j81blog/Citrix_Optimizer_Community_Template_Marketplace/master/templates/John%20Billekens/JohnBillekens_3rd_Party_Components.xml" -UseBasicParsing -OutFile "$OptimizerHome\Templates\JohnBillekens_3rd_Party_Components.xml"
 #//  Remove execution if using BIS-F
-& "$OptimizerHome\CtxOptimizerEngine.ps1" -Template "$OptimizerHome\Templates\JohnBillekens_3rd_Party_Components.xml" -mode Execute 
+# & "$OptimizerHome\CtxOptimizerEngine.ps1" -Template "$OptimizerHome\Templates\JohnBillekens_3rd_Party_Components.xml" -mode Execute 
