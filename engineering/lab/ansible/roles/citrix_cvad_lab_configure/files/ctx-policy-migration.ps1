84a0476f7154c07e7add1bb0aad87904b2ee58f5
jameskindon <James.Kindon@nutanix.com>
2023-01-10 14:11:22 +1100

    cvad simple build additions

---
+++ engineering/lab/ansible/roles/citrix_cvad_lab_configure/files/ctx-policy-migration.ps1
@@ -0,0 +1,28 @@
+Param(
+     [Parameter(Mandatory = $True)]
+     [ValidateSet("Export","Import")]
+     $Mode,
+     
+     [Parameter(Mandatory = $True)]
+     [ValidateSet("1912_LTSR","2203_LTSR","CR")]
+     $BuildVersion,
+
+     [Parameter(Mandatory = $False)]
+     $PolicyDataLocation = "\\10.57.64.39\MDTLoginVSI$\Applications\CitrixPolicyStore\$BuildVersion",
+
+     [Parameter(Mandatory = $False)]
+     $PolicyDataFile = "SitePolicySet.txt"
+ )
+
+
+if ($Mode -eq "Export") {
+    if (!(Test-Path $PolicyDataLocation)){ New-Item -Path $PolicyDataLocation -ItemType Directory -Force | Out-Null }
+    Add-PSSnapin Citrix*
+    Export-BrokerDesktopPolicy | Out-File "$PolicyDataLocation\$PolicyDataFile" -Force
+}
+
+if ($Mode -eq "Import") {
+    if (!(Test-Path $PolicyDataLocation)){ Write-Warning "No source files detected. Exit Script"; Exit 1 }
+    Add-PSSnapin Citrix*
+    Import-BrokerDesktopPolicy (Get-Content "$PolicyDataLocation\$PolicyDataFile")
+}
