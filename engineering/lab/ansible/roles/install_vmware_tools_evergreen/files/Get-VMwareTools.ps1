0d735650eaccf79c4c48e9840b3b822e1794ffaf
keesbaggerman <90334315+kbaggerman-ntnx@users.noreply.github.com>
2023-11-13 23:25:36 +0100

    Update Get-VMwareTools.ps1

--- engineering/lab/ansible/roles/install_vmware_tools_evergreen/files/Get-VMwareTools.ps1
+++ engineering/lab/ansible/roles/install_vmware_tools_evergreen/files/Get-VMwareTools.ps1
@@ -7,13 +7,13 @@ if ((Get-WmiObject win32_operatingsystem | select osarchitecture).osarchitecture
     #64 bit logic here
     Write "64-bit OS"
     WGET $VmwareTools[0].uri -OutFile C:\Install\VMwareTools.exe
-    C:\Install\VMwareTools_x64.exe /S /v "/qn REBOOT=R ADDLOCAL=ALL REMOVE=Hgfs,CBHelper,FileIntrospection,NetworkIntrospection,ServiceDiscovery,DeviceHelper"
+    # C:\Install\VMwareTools_x64.exe /S /v "/qn REBOOT=R ADDLOCAL=ALL REMOVE=Hgfs,CBHelper,FileIntrospection,NetworkIntrospection,ServiceDiscovery,DeviceHelper"
 }
 else
 {
     #32 bit logic here
     Write "32-bit OS"
     WGET $VmwareTools[1].uri -OutFile C:\Install\VMwareTools.exe
-    C:\Install\VMwareTools_x86.exe /S /v "/qn REBOOT=R ADDLOCAL=ALL REMOVE=Hgfs,CBHelper,FileIntrospection,NetworkIntrospection,ServiceDiscovery,DeviceHelper"
+    # C:\Install\VMwareTools_x86.exe /S /v "/qn REBOOT=R ADDLOCAL=ALL REMOVE=Hgfs,CBHelper,FileIntrospection,NetworkIntrospection,ServiceDiscovery,DeviceHelper"
 }
 
