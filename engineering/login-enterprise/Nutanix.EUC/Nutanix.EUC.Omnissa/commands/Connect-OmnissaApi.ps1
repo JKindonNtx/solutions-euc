6156cde90f8eae83df418c0bceee0043a2c19499
ntnxDave <david.brett@nutanix.com>
2024-07-02 03:20:13 -0700

    Update Connect-OmnissaApi.ps1

--- engineering/login-enterprise/Nutanix.EUC/Nutanix.EUC.Omnissa/commands/Connect-OmnissaApi.ps1
+++ engineering/login-enterprise/Nutanix.EUC/Nutanix.EUC.Omnissa/commands/Connect-OmnissaApi.ps1
@@ -12,5 +12,73 @@ function Connect-OmnissaApi(){
         domain = $domain
     }
 
-    return invoke-restmethod -Method Post -uri "$url/rest/login" -ContentType "application/json" -Body ($Credentials | ConvertTo-Json) -SkipCertificateCheck
+    $Path = "$($ApiEndpoint)/rest/login"
+
+    if ($PSEdition -eq "Core") {
+        $count = 0
+        $maxcount = 5
+        $done = $false
+        while ($done -eq $false) {
+            $count++
+            try {
+                $URL = $Path
+                $Return = Invoke-RestMethod -ContentType "application/json" -Body ($Credentials | ConvertTo-Json) -Method "POST" -Uri $URL -SkipCertificateCheck
+                $done = $true
+            }
+            catch {
+                $reason = $_
+                Write-Log -Message "API call failed, sleeping 2 seconds and trying again $($maxcount - $count) times: $_" -Level Warn
+                Start-Sleep -Seconds 2
+            }
+            if ($count -eq $maxcount) {
+                Write-Log -Message "API call failed after $($maxcount) times with reason: $reason" -Level Error
+                Exit 1
+            }
+        }
+    }
+    else {
+        if (-not("SSLValidator" -as [type])) {
+            add-type -TypeDefinition @"
+        using System;
+        using System.Net;
+        using System.Net.Security;
+        using System.Security.Cryptography.X509Certificates;
+
+        public static class SSLValidator {
+            public static bool ReturnTrue(object sender,
+                X509Certificate certificate,
+                X509Chain chain,
+                SslPolicyErrors sslPolicyErrors) { return true; }
+
+            public static RemoteCertificateValidationCallback GetDelegate() {
+                return new RemoteCertificateValidationCallback(SSLValidator.ReturnTrue);
+            }
+        }
+"@
+        }
+        [System.Net.ServicePointManager]::ServerCertificateValidationCallback = [SSLValidator]::GetDelegate()
+        $count = 0
+        $maxcount = 5
+        $done = $false
+        while ($done -eq $false) {
+            $count++
+            try {
+                $URL = $Path
+                $Return = Invoke-RestMethod -ContentType "application/json" -Body ($Credentials | ConvertTo-Json) -Method "POST" -Uri $URL
+                $done = $true
+            }
+            catch {
+                $reason = $_
+                Write-Log "API call $url failed, sleeping 2 seconds and trying again $($maxcount - $count) times: $_" -Level Warn
+                Start-Sleep -Seconds 2
+            }
+            if ($count -eq $maxcount) {
+                Write-Log -Message "API call failed after $($maxcount) times with reason: $reason" -Level Error
+                Exit 1
+            }
+        }
+    }
+
+    return $Return
+
 }
